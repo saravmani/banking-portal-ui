623d5487b7c88ff0048537f992346586bbc47230
saravana <saravmanicoud@gmail.com>
2025-02-08 19:28:31 +0530

    fix

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://retry-api-app1-build-winnerrs-mani-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
+  apiUrl: 'https://bankapi-prod-winnerrs-mani-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
   tokenName :  "authToken",
   origin:'http://localhost:4200' 
 };
